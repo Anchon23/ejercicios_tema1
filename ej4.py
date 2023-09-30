cb0064e87e4ec31b1e4aa6e0abc861ff5a7b13f1
Anchon23 <angellopezrouanet@gmail.com>
2023-09-30 15:31:42 +0200

    Update ej4.py

---
+++ ej4.py
@@ -0,0 +1,15 @@
+def function(ip):
+    lista = ip.split(".")
+    if len(lista) == 4:
+        for i in lista:
+            try:
+                if int(i) < 0 or int(i) > 255:
+                    return False
+            except ValueError as e:
+                e = "alguno de los valores no es un numero"
+                return e
+        return True
+    else:
+        return False
+
+print(function("111.231.23.12"))
